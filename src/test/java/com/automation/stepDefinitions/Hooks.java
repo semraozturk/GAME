c2a4be6d401e1989c16885566ceb6891ec266574
Semra Yilmaz <66539468+semraozturk@users.noreply.github.com>
2024-06-21 15:46:19 -0400

    Update Hooks.java

--- src/test/java/com/automation/stepDefinitions/Hooks.java
+++ src/test/java/com/automation/stepDefinitions/Hooks.java
@@ -5,6 +5,9 @@ import com.automation.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import java.util.Arrays;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ImpersonatedCredentials;
+import com.google.cloud.bigquery.*;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ImpersonatedCredentials;
@@ -26,38 +29,11 @@ public class Hooks {
         //Driver.closeDriver();
         
         String projectId = "imposing-timer-420716";
-        String scope = "https://www.googleapis.com/auth/cloud-platform";
-
-        // The name of the privilege-bearing service account for whom the credential is created.
         String impersonatedServiceAccount = "automation@imposing-timer-420716.iam.gserviceaccount.com";
+        GoogleCredentials googleCredentials = GoogleCredentials.getApplicationDefault();
+        BigQuery bigquery = BigQueryOptions.newBuilder().setCredentials(googleCredentials).setProjectId(projectId).build().getService();
+        System.out.println("after big query");
 
-            // Construct the GoogleCredentials object which obtains the default configuration from your
-            // working environment.
-            GoogleCredentials googleCredentials = GoogleCredentials.getApplicationDefault();
-
-            // delegates: The chained list of delegates required to grant the final accessToken.
-            // For more information, see:
-            // https://cloud.google.com/iam/docs/create-short-lived-credentials-direct#sa-credentials-permissions
-            // Delegate is NOT USED here.
-            List<String> delegates = null;
-
-            // Create the impersonated credential.
-            ImpersonatedCredentials impersonatedCredentials =
-                    ImpersonatedCredentials.newBuilder()
-                            .setSourceCredentials(googleCredentials)
-                            .setTargetPrincipal(impersonatedServiceAccount)
-                            .setScopes(Arrays.asList(scope))
-                            .setLifetime(300)
-                            .setDelegates(delegates)
-                            .build();
-
-            // Get the OAuth2 token.
-            // Once you've obtained the OAuth2 token, you can use it to make an authenticated call.
-            impersonatedCredentials.refresh();
-            String accessToken = impersonatedCredentials.getAccessToken().getTokenValue();
-            System.out.println("Generated access token." + accessToken);
-
-        
     }
 
 
